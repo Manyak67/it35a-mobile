e1ec9c48158bbd81c19a59516adc1c5c6746fb1a
Manyak67 <tulodchristainjoshua1@gmail.com>
2026-03-30 09:45:45 +0800

    Initial release

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -9,7 +9,6 @@ import {
   IonCardHeader, 
   IonCardSubtitle, 
   IonCardTitle } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
