c112e8533a4d5a9b7ca425d1f8dbbcedc8045822
Specter-009 <xvr09200@gmail.com>
2026-03-30 11:28:51 +0800

    ay

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Kurasoy</IonTitle>
+          <IonTitle>whats up HOMIE</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
