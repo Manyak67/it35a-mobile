f1d361b586c951a6364d1a52c1606192e759907e
Manyak67 <tulodchristainjoshua1@gmail.com>
2026-04-06 16:18:41 +0800

    feat: convert sidebar to toggleable overlay and add menu button

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,17 +1,16 @@
-import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar,  } from "@ionic/react"
+import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar, } from "@ionic/react"
 import { homeOutline, logOutOutline } from "ionicons/icons"
 import { Redirect, Route } from "react-router-dom"
 import Home from "./Home"
 
 const Menu: React.FC = () => {
     const path = [
-        {name: 'Home', url:'/app/home', icon:homeOutline}
+        { name: 'Home', url: '/app/home', icon: homeOutline }
 
     ];
     return (
         <IonPage>
-            <IonSplitPane contentId="main">
-            <IonMenu contentId="main">
+            <IonMenu contentId="main" type="overlay">
                 <IonHeader>
                     <IonToolbar>
                         <IonTitle>Menu</IonTitle>
@@ -25,23 +24,23 @@ const Menu: React.FC = () => {
                             <IonItem routerLink={item.url} routerDirection="forward">
                                 <IonIcon icon={item.icon}
                                     slot="start"></IonIcon>
-                                    {item.name}
+                                {item.name}
                             </IonItem>
                         </IonMenuToggle>
                     ))}
-                    <IonButton routerLink="/" routerDirection="back" expand ="full">
+
+                    <IonButton routerLink="/" routerDirection="back" expand="full">
                         <IonIcon icon={logOutOutline} slot="start"></IonIcon>
                         Logout
                     </IonButton>
                 </IonContent>
-                </IonMenu>
-                <IonRouterOutlet id="main">
-                    <Route exact path="/app/home" component={Home} />
-                    <Route exact path="/app">
-                    <Redirect to="/app/home" /> 
-                    </Route>
-                </IonRouterOutlet>
-            </IonSplitPane>
+            </IonMenu>
+            <IonRouterOutlet id="main">
+                <Route exact path="/app/home" component={Home} />
+                <Route exact path="/app">
+                    <Redirect to="/app/home" />
+                </Route>
+            </IonRouterOutlet>
         </IonPage>
 
     );
