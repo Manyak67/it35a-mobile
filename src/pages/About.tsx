157a90ad5c9569c70ed80a304c36072b72216ece
Manyak67 <tulodchristainjoshua1@gmail.com>
2026-04-13 08:01:41 +0800

    Added About.tsx to pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,28 @@
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const About: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>About Us</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name="About page" />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default About;
